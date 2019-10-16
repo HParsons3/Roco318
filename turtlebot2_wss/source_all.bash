412f827d874ead5594a6d63d0bf7c26c19ac6c40
evilparsnips <harry.parsons@talktalk.net>
2019-10-16 20:17:33 +0100

    Added new .ttt file

--- turtlebot2_wss/source_all.bash
+++ turtlebot2_wss/source_all.bash
@@ -1,8 +1,8 @@
 #! /bin/bash 
 
-TURTLEBOT_DRIVERS_WS=~/Documents/turtlebot2_wss/turtlebot_ws
-TURTLEBOT_SIMULATION_WS=~/Documents/turtlebot2_wss/turtlebot_simulation
-TURTLEBOT_INTERACTION_WS=~/Documents/turtlebot2_wss/turtlebot_interaction
+TURTLEBOT_DRIVERS_WS=~/Roco318/turtlebot2_wss/turtlebot_ws
+TURTLEBOT_SIMULATION_WS=~/Roco318/turtlebot2_wss/turtlebot_simulation
+TURTLEBOT_INTERACTION_WS=~/Roco318/turtlebot2_wss/turtlebot_interaction
 
 source $TURTLEBOT_DRIVERS_WS/devel/setup.bash --extend
 source $TURTLEBOT_SIMULATION_WS/devel/setup.bash --extend
